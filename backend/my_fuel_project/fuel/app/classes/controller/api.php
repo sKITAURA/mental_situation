ce407368c47384bf4f50418a29c06f06f8e4b532
sKITAURA <progrmingmitsuba@gmail.com>
2024-11-01 15:59:23 +0900

    bag fix

--- backend/my_fuel_project/fuel/app/classes/controller/api.php
+++ backend/my_fuel_project/fuel/app/classes/controller/api.php
@@ -3,25 +3,26 @@
 use Fuel\Core\Controller_Rest;
 use Fuel\Core\Input;
 
-class Controller_Api extends Controller
+class Controller_Api extends Controller_Rest
 {
     protected $format = 'json';
 
-    // public function before()
-    // {
-    //     parent::before();
+    public function before()
+    {
+        parent::before();
 
-    //     // CORS ヘッダーの設定
-    //     $this->response->set_header('Access-Control-Allow-Origin', 'http://localhost:5174');
-    //     $this->response->set_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    //     $this->response->set_header('Access-Control-Allow-Headers', 'Content-Type, X-Requested-With');
+        // CORS ヘッダーの設定
+        $this->response->set_header('Access-Control-Allow-Origin', 'http://localhost:5174');
+        $this->response->set_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+        $this->response->set_header('Access-Control-Allow-Headers', 'Content-Type, X-Requested-With');
+        $this->response->set_header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
-    //     // プリフライトリクエストへの対応
-    //     if (Input::method() === 'OPTIONS') {
-    //         $this->response->set_status(204);
-    //         return $this->response;
-    //     }
-    // }
+        // プリフライトリクエストへの対応
+        if (Input::method() === 'OPTIONS') {
+            $this->response->set_status(204);
+            return $this->response;
+        }
+    }
     public function get_get_data(){
         
         $data = Model_Situation::get_situation_data();
